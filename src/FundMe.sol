911dcbd8b613ef8154178c22f8273a972439c4b6
4rjunc <arjunmundamani@gmail.com>
2025-03-18 20:33:17 +0530

    Setting up functions

---
+++ src/FundMe.sol
@@ -0,0 +1,14 @@
+// Get funds from users
+// Withdraw funds
+// Set a minimum funding value in USD
+
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.13;
+
+contract FundMe {
+  function fund() public {}
+
+  function withdraw() public {}
+
+}
