855c76baa47ba210f2500dab334f13f3fd4c822f
4rjunc <arjunmundamani@gmail.com>
2025-03-18 23:13:30 +0530

    Minor

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -7,12 +7,14 @@
 pragma solidity ^0.8.13;
 
 contract FundMe {
+  
+  uint256 public minimumUSD = 5
+
   function fund() public payable{
     // Allow users to send $
     // Have a minimum $ sent 
     // How to send ETH to a contract
-    require(msg.value > 1e18, "didn't send enough ETH"); // 1e18 = 1 ETH 
-
+    require(msg.value > minimumUSD, "didn't send enough ETH"); // 1e18 = 1 ETH 
   }
 
   function withdraw() public {}
