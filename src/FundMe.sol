1bd8a94368022858035a910cfa72d6579da5671f
4rjunc <arjunmundamani@gmail.com>
2025-03-19 01:00:22 +0530

    Initial Version of Sepolia Oracle

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -6,12 +6,27 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
 
 
 contract FundMe {
-    function fund()public  payable  {
-        require(msg.value >= 1e18, "not enough");
+    uint256 public minimumUSD = 5e18;
 
+    function fund()public  payable  {
+        require(getConversionRate(msg.value) >= minimumUSD, "not enough");
     }
 
-    function getVersion() public view  returns (uint256) {
-        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    function getPrice() public view returns (uint256) {
+        AggregatorV3Interface price =  AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (
+            /* uint80 roundID */,
+            int answer,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        )= price.latestRoundData();
+        return uint256(answer) * 1e18;
+    }
+
+    function getConversionRate(uint256 ethAmount) public  view  returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
     }
 }
