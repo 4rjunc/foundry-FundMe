6fcd02987521521fb63162fddfbf0138bb9bc652
4rjunc <arjunmundamani@gmail.com>
2025-03-18 21:05:43 +0530

    Added requires

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -7,7 +7,13 @@
 pragma solidity ^0.8.13;
 
 contract FundMe {
-  function fund() public {}
+  function fund() public payable{
+    // Allow users to send $
+    // Have a minimum $ sent 
+    // How to send ETH to a contract
+    require(msg.value > 1e18, "didn't send enough ETH"); // 1e18 = 1 ETH 
+
+  }
 
   function withdraw() public {}
 
